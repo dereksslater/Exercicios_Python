d4b187327928f04bc96067234a95bb660351b035
Derek Slater <linsslater072@gmail.com>
2026-07-01 15:49:17 -0300

    feat: Resolvendo exercício antigo

---
+++ ExerciciosEmPython/exerc5.py
@@ -0,0 +1,7 @@
+#Dado um número inteiro com mais do que 1 dígito, elabore uma expressão que resulte no último dígito do número. Exemplos:
+
+valor = 2026
+
+ultimoDigito = valor %10
+
+print(ultimoDigito)
