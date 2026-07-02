a37dc9ff0f63a5ef7974a5ad19b9e11e5060d966
Derek Slater <linsslater072@gmail.com>
2026-07-02 13:31:19 -0300

    feat: (06 ex) Resolve exercício lista(condição)

---
+++ ExerciciosDeCondicao/exercicioCond6.py
@@ -0,0 +1,9 @@
+# Obtenha um valor numérico via entrada do usuário e apresente um valor Lógico indicando se o valor é par.
+
+number = int (input("Digite um número: "))
+
+if number % 2 == 0 :
+    print("O número é par")
+else: 
+    print("O número é ímpar")
+    
