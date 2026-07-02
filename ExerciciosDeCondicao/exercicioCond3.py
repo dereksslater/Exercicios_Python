d3ac1a62f82b7e20e00ee411480fd363e39d172a
Derek Slater <linsslater072@gmail.com>
2026-07-02 11:38:08 -0300

    Exercicío 3 concluído

---
+++ ExerciciosDeCondicao/exercicioCond3.py
@@ -0,0 +1,8 @@
+#Obtenha um valor numérico via entrada do usuário e apresente um valor Lógico indicando se o valor está no intervalo 0<x<1. Separe a verificação em duas partes (utilizando um operador lógico).
+
+numb = float(input("Digite um número: "))
+
+if numb > 0 and numb < 1:
+    print(f" O valor {numb} está dentro de 0 a 1")
+else:
+    print(f" O valor {numb} não esta dentro de 0 a 1")
