7f0ae21b1cfbc4b100cec037ecb317e502bbf0d4
Derek Slater <linsslater072@gmail.com>
2026-07-02 13:24:22 -0300

    feat:(05 ex) Resolve exercício Lista(condição)

---
+++ ExerciciosDeCondicao/exercicioCond5.py
@@ -0,0 +1,13 @@
+#Obtenha três valores numéricos via entrada do usuário e apresente um valor Lógico indicando se os valores foram informados em ordem crescente.
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+Crescente = num1 <= num2 <= num3
+
+if Crescente :
+    print(f"Os valores foram digitado em ordem crescente, os valores foram: {num1},{num2},{num3}")
+else:
+    print(f"Os valores não foram digitados em ordem crescente, os valores foram: {num1},{num2},{num3}")
+    
