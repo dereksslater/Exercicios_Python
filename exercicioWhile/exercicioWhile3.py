e4356797c433c62122122c588a26ce7bf11037e4
Derek Slater <linsslater072@gmail.com>
2026-07-03 15:15:47 -0300

    feat:(03 ex) exercício resolvido (While)

---
+++ exercicioWhile/exercicioWhile3.py
@@ -0,0 +1,17 @@
+#3. Obtenha via entrada do usuário um número inteiro positivo qualquer e exiba a tabuada desse número. Apresente da seguinte forma (exemplo com o número 2 como entrada):
+#2 x 1 = 2
+#2 x 2 = 4
+#2 x 10 = 20 
+
+n1 = int(input("Digite um número: "))
+
+
+contador = 1
+
+if n1 <1 :
+    print("Erro: número negativo não pode ser escrito")
+else:   
+  while contador <=10:
+       resultado = n1 * contador
+       print(f"{n1} x {contador} = {resultado}")
+       contador = contador + 1 
