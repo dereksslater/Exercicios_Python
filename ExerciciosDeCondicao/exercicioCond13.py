fe9c6e0f9df218df1e53a4dd354b69c708b77dc7
Derek Slater <linsslater072@gmail.com>
2026-07-03 10:25:17 -0300

    feat: (13 ex) exercício resolvido (Condição)

---
+++ ExerciciosDeCondicao/exercicioCond13.py
@@ -0,0 +1,13 @@
+#Obtenha dois valores numéricos via entrada do usuário e apresente qual dos dois valores é o maior. Caso os valores sejam iguais, avise o usuário e não informe qual número é o maior.
+valor1 = int(input("Digite o primeiro número: "))
+valor2 = int(input("Digite o segundo número: "))
+
+if valor1 > valor2:
+    print(f"O valor {valor1} é maior que o {valor2}")
+    
+elif valor1 < valor2:
+    print(f"O valor {valor2} é maior que o {valor1}") 
+    
+elif valor1 == valor2:
+    print("Os valores são iguais") 
+      
