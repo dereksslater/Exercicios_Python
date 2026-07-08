9ab1fcbac27e213b13315592ad4decacba6b54a1
Derek Slater <linsslater072@gmail.com>
2026-07-08 11:48:24 -0300

    feat: (03 ex) exercício resolvido (Função)

---
+++ ExerciciosDeFuncoes/exercicioFuncao3.py
@@ -0,0 +1,4 @@
+#Utilize a função do exercício 1 ou 2 na construção de uma função que verifica se uma nota de prova é válida. As notas de provas precisam ser valores entre 0 e 10, incluindo-se o 0 e 10. A entrada desta função é apenas um número representando a nota de uma prova e o retorno é um valor lógico indicando se a nota é válida ou não.
+import exercicioFuncao1
+
+print(exercicioFuncao1.intervalo(0, 10, 7))
