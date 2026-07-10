21a577c7af9182c95153b3782c371b323e24357e
Derek Slater <linsslater072@gmail.com>
2026-07-10 15:29:49 -0300

    feat: (04 ex) exercício resolvido (listas)

---
+++ ExerciciosDeListas/exercicioListas4.py
@@ -0,0 +1,19 @@
+#Recebe uma lista de números e retorna o somatório dos números pares presentes na lista.
+
+
+def somar_pares(lista):
+    total = 0
+    for num in lista:
+        if num % 2 == 0:
+            total = total + num 
+            
+    return total 
+
+
+minha_lista = [1, 2, 3, 4, 5, 6, 4, 4, 7, 78, 22]
+
+
+resultado = somar_pares(minha_lista)
+
+
+print(resultado)
