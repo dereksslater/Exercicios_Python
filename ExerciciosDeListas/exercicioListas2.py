aa35f762bf433e085297c355bd496f0ffb78bba4
Derek Slater <linsslater072@gmail.com>
2026-07-10 14:24:16 -0300

    feat: (02 ex) exercício resolvido (Lista)

---
+++ ExerciciosDeListas/exercicioListas2.py
@@ -0,0 +1,19 @@
+# Recebe uma lista de números e retorna quantos números primos a lista possui. Importe sua função para verificação de primos diretamente do módulo em que ela se encontra (arquivo .py) e utilize-a para resolver esse exercício.
+from funcaoVerificaPrimos import eh_primo
+
+lista = [1, 2, 4, 5, 6, 7]
+ind = 0
+total = 0
+tam = len(lista)
+
+while ind < tam: 
+    numero_atual = lista[ind] 
+    
+    if eh_primo(numero_atual):
+        total = total + 1
+    
+    ind = ind + 1 
+
+print(total)
+   
+    
