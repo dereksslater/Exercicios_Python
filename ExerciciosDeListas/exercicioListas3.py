0809adcaac8343c88cbf0e75c69c0e18ff598bd3
Derek Slater <linsslater072@gmail.com>
2026-07-10 14:47:20 -0300

    feat: (03 ex) exercício resolvido (Listas)

---
+++ ExerciciosDeListas/exercicioListas3.py
@@ -0,0 +1,10 @@
+# Recebe uma lista com conteúdos de diferentes tipos e mostra-os para o usuário em ordem inversa (essa função não tem retorno).
+
+def mostrar_lista_inversa(lista):
+ for item in reversed(lista):
+  print(item)
+
+
+minha_lista = ["Derek", 26, "Brasil","outubro",23,56,55,44,44]
+
+mostrar_lista_inversa(minha_lista)  
