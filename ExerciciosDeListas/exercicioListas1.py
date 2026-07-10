43410c64f681963a35a0b13c9c4f0086c7e3a927
Derek Slater <linsslater072@gmail.com>
2026-07-10 10:53:02 -0300

    feat: (01 ex) exercício resolvido (Listas)

---
+++ ExerciciosDeListas/exercicioListas1.py
@@ -0,0 +1,23 @@
+# Recebe uma lista de números e retorna qual o maior número presente na lista.
+
+def MaiorLista(a,b,c,d,e,f,g):
+    lista = [a,b,c,d,e,f,g]
+    ind = 0
+    numeroMaior = lista [0]
+    tam = len(lista)
+    
+    while ind < tam:
+            if lista[ind] > numeroMaior:
+             numeroMaior = lista[ind]
+            ind = ind + 1     
+    
+    return numeroMaior
+    
+    
+print(MaiorLista(3,2,5,6,3,5,5))                   
+            
+
+               
+
+    
+       
