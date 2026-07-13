3564794ddb97a921d1b1467846312a1c54be40a4
Derek Slater <linsslater072@gmail.com>
2026-07-13 11:33:19 -0300

    feat: (07 ex) exercício resolvido (listas)

---
+++ ExerciciosDeListas/exercicioListas7.py
@@ -0,0 +1,14 @@
+# Recebe uma lista de números e apresenta para o usuário o fatorial de cada um deles (utilize a função factorial do módulo math). 
+import math
+
+def lista_fatorial(lista):
+    ind = 0
+    lista[ind]
+    while ind < len(lista):
+        total = math.factorial(lista[ind])
+        print(total)
+        ind = ind + 1
+        
+          
+
+print(lista_fatorial([3,4,5,7,5,5,2,5,2,1,1,0]))
