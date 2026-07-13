b8500d82ada8959b8c6a3b4ac8776baa1fcbc265
Derek Slater <linsslater072@gmail.com>
2026-07-13 14:37:18 -0300

    feat: (08 ex) exercício resolvido (listas)

---
+++ ExerciciosDeListas/exercicioListas8.py
@@ -0,0 +1,18 @@
+# Recebe uma lista com notas (valores em ponto flutuante) entre 0 e 10 e retorna uma lista contendo a média e a moda das notas. Para o cálculo da média e mediana, utilize funções do módulo statistics. Caso a lista contenha notas inválidas, não realize o cálculo e a função não retorna nada.
+import statistics # mode, mean
+
+def media_moda(lista):
+    ind = 0
+    while ind <len(lista):
+     if lista[ind] <0 or lista[ind] >10:
+         ind = ind + 1
+         return None
+     else:
+            moda = statistics.mode(lista)
+            media = statistics.mean(lista)
+            ind = ind + 1
+    print(f"A média é: {media}, e a moda é: {moda}")   
+   
+    
+    
+print(media_moda([2.3,5.4,3.4,5.5,5.5,2.3,5,10]))
